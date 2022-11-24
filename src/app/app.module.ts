70ce4fe6646b076e355f405f2d80f59ab9ae0d86
Ruban Moses <ruban.moses@exathought.com>
2022-11-24 14:45:04 +0530

    completed question 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { DocumentTableDateComponent } from './document-table-date/document-table
 import { DocumentTableStatusComponent } from './document-table-status/document-table-status.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { GridLayoutComponent } from './grid-layout/grid-layout.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
     DocumentTableNameComponent,
     DocumentTableDateComponent,
     DocumentTableStatusComponent,
-    LoginFormComponent
+    LoginFormComponent,
+    GridLayoutComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/grid-layout/grid-layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grid-layout',
+  templateUrl: './grid-layout.component.html',
+  styleUrls: ['./grid-layout.component.css']
+})
+export class GridLayoutComponent {
+
+}
